54d31862bb26bc54ed80ba52fa20dca1c8a83b7b
Iwein Bau <iweinbau@MacBook-Pro-van-Iwein.local>
2018-10-26 18:57:14 +0200

    Make is visual studio independent

--- Ray tracing/Ray tracing/main.cpp
+++ Ray tracing/Ray tracing/main.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <math.h>
 #include <algorithm>
@@ -43,83 +43,83 @@ private:
 
 class Ray {
 public:
-	Ray(Vect3f origin, Vect3f direction) {
-		this->origin = origin;
-		this->direction = direction;
-	}
-	Vect3f getOrgin() {
-		return origin;
-	}
-	Vect3f getDirection() {
-		return direction;
-	}
+    Ray(Vect3f origin, Vect3f direction) {
+        this->origin = origin;
+        this->direction = direction;
+    }
+    Vect3f getOrgin() {
+        return origin;
+    }
+    Vect3f getDirection() {
+        return direction;
+    }
 private:
-	//the origin point of the ray.
-	Vect3f origin;
-	//the direction the ray is going.
-	Vect3f direction;
+    //the origin point of the ray.
+    Vect3f origin;
+    //the direction the ray is going.
+    Vect3f direction;
 };
 
 class Camera {
 public:
-	//resolution in pixels.
-	const int width = 600, height = 600;
-	Camera(Vect3f lookfrom, Vect3f lookat, float fov) { // fov is top to bottom in degrees
-														//the aspect ration of the image.
-		float aspect = width / height;
-		//the definition of the up vector of the world.
-		Vect3f up(0, 1, 0);
-		//fov in radians.
-		float theta = fov * PI / 180;
-		//half the height of the image view. 
-		float half_height = tan(theta / 2);
-		//half the width of the image view.
-		float half_width = aspect * half_height;
-		//the position.
-		origin = lookfrom;
-		// calculate the direction vector
-		n = (lookfrom - lookat).normalized();
-		// calculate the horizontal vector of the camera.
-		u = up.cross(n).normalized();
-		//calculate the up vector.
-		v = n.cross(u);
-		//  calculate the lower left corner;
-		lower_left_corner = origin - u * half_width - v * half_height - n;
-		// horizontal vector.
-		horizontal = u * half_width * 2;
-		//vertical vector.
-		vertical = v * 2 * half_height;
-	}
-	Ray constructRay(int i, int j) {
-		//get the pixel size.
-		float x = float(i) / float(width);
-		float y = float(j) / float(height);
-		//calculate the ray.
-		return Ray(origin, (lower_left_corner + horizontal * x + vertical * y - origin).normalized());
-	}
+    //resolution in pixels.
+    const int width = 600, height = 600;
+    Camera(Vect3f lookfrom, Vect3f lookat, float fov) { // fov is top to bottom in degrees
+        //the aspect ration of the image.
+        float aspect = width / height;
+        //the definition of the up vector of the world.
+        Vect3f up(0, 1, 0);
+        //fov in radians.
+        float theta = fov * PI / 180;
+        //half the height of the image view.
+        float half_height = tan(theta / 2);
+        //half the width of the image view.
+        float half_width = aspect * half_height;
+        //the position.
+        origin = lookfrom;
+        // calculate the direction vector
+        n = (lookfrom - lookat).normalized();
+        // calculate the horizontal vector of the camera.
+        u = up.cross(n).normalized();
+        //calculate the up vector.
+        v = n.cross(u);
+        //  calculate the lower left corner;
+        lower_left_corner = origin - u * half_width - v * half_height - n;
+        // horizontal vector.
+        horizontal = u * half_width * 2;
+        //vertical vector.
+        vertical = v * 2 * half_height;
+    }
+    Ray constructRay(int i, int j) {
+        //get the pixel size.
+        float x = float(i) / float(width);
+        float y = float(j) / float(height);
+        //calculate the ray.
+        return Ray(origin, (lower_left_corner + horizontal * x + vertical * y - origin).normalized());
+    }
 private:
-	//the position of the camera;
-	Vect3f origin;
-	//the lower left corner of the view plane.
-	Vect3f lower_left_corner;
-	//the horizontal vector/of the image view.
-	Vect3f horizontal;
-	//the vertical vector of the image view.
-	Vect3f vertical;
-	//unit vectors of the camera axis.
-	Vect3f u, v, n;
+    //the position of the camera;
+    Vect3f origin;
+    //the lower left corner of the view plane.
+    Vect3f lower_left_corner;
+    //the horizontal vector/of the image view.
+    Vect3f horizontal;
+    //the vertical vector of the image view.
+    Vect3f vertical;
+    //unit vectors of the camera axis.
+    Vect3f u, v, n;
 };
 class Object {
 public:
-	Vect3f position;
-	virtual bool intersect(Ray ray, Vect3f &intersection, float &t) =0;
-	virtual Vect3f getNormalAtPoint(Vect3f) = 0;
-	virtual Vect3f getPosition() = 0;
-	Material getMaterial() {
-		return material;
-	}
+    Vect3f position;
+    virtual bool intersect(Ray ray, Vect3f &intersection, float &t) =0;
+    virtual Vect3f getNormalAtPoint(Vect3f) = 0;
+    virtual Vect3f getPosition() = 0;
+    Material getMaterial() {
+        return material;
+    }
 protected:
-	Material material;
+    Material material;
 };
 class Plane : public Object{
 public:
@@ -157,94 +157,94 @@ private:
 };
 class Sphere : public Object{
 public:
-	Sphere(){}
-	Sphere(Vect3f position, float radius,Material material){
-		this->position = position;
-		this->radius = radius;
-		this->material = material;
-	}
-	bool intersect(Ray ray,Vect3f &instersectionPoint,float &t) {
-		float t0, t1; // solutions for t if the ray intersects 
-					  // analytic solution
-		Vect3f L = ray.getOrgin() - position;
-		float a = ray.getDirection().dot(ray.getDirection());
-		float b = 2 * ray.getDirection().dot(L);
-		float c = L.dot(L) - radius;
-		if (!solveQuadratic(a, b, c, t0, t1)) return false;
-
-		if (t0 > t1) std::swap(t0, t1);
-
-		if (t0 < 0) {
-			t0 = t1; // if t0 is negative, let's use t1 instead 
-			if (t0 < 0) return false; // both t0 and t1 are negative 
-		}
-
-		t = t0;
-		instersectionPoint = ray.getOrgin() + ray.getDirection() * t;
-
-		return true;
-	}
-	Vect3f getNormalAtPoint(Vect3f point) {
-		return (point - position).normalized();
-	}
-	Vect3f getPosition() {
-		return position;
-	}
+    Sphere(){}
+    Sphere(Vect3f position, float radius,Material material){
+        this->position = position;
+        this->radius = radius;
+        this->material = material;
+    }
+    bool intersect(Ray ray,Vect3f &instersectionPoint,float &t) {
+        float t0, t1; // solutions for t if the ray intersects
+        // analytic solution
+        Vect3f L = ray.getOrgin() - position;
+        float a = ray.getDirection().dot(ray.getDirection());
+        float b = 2 * ray.getDirection().dot(L);
+        float c = L.dot(L) - radius;
+        if (!solveQuadratic(a, b, c, t0, t1)) return false;
+        
+        if (t0 > t1) std::swap(t0, t1);
+        
+        if (t0 < 0) {
+            t0 = t1; // if t0 is negative, let's use t1 instead
+            if (t0 < 0) return false; // both t0 and t1 are negative
+        }
+        
+        t = t0;
+        instersectionPoint = Vect3f(ray.getOrgin() + ray.getDirection() * t);
+        
+        return true;
+    }
+    Vect3f getNormalAtPoint(Vect3f point) {
+        return (point - position).normalized();
+    }
+    Vect3f getPosition() {
+        return position;
+    }
 private:
-	bool solveQuadratic(const float &a, const float &b, const float &c, float &x0, float &x1)
-	{
-		float discr = b * b - 4 * a * c;
-		if (discr < 0) return false;
-		else if (discr == 0) {
-			x0 = x1 = -0.5 * b / a;
-		}
-		else {
-			float q = (b > 0) ?
-				-0.5 * (b + sqrt(discr)) :
-				-0.5 * (b - sqrt(discr));
-			x0 = q / a;
-			x1 = c / q;
-		}
-
-		return true;
-	}
-	float radius;
+    bool solveQuadratic(const float &a, const float &b, const float &c, float &x0, float &x1)
+    {
+        float discr = b * b - 4 * a * c;
+        if (discr < 0) return false;
+        else if (discr == 0) {
+            x0 = x1 = -0.5 * b / a;
+        }
+        else {
+            float q = (b > 0) ?
+            -0.5 * (b + sqrt(discr)) :
+            -0.5 * (b - sqrt(discr));
+            x0 = q / a;
+            x1 = c / q;
+        }
+        
+        return true;
+    }
+    float radius;
 };
 class Light {
-	public:
-		Light(Vect3f diffuse, Vect3f specular, Vect3f position) {
-			this->diffuse = diffuse;
-			this->specular = specular;
-			this->position = position;
-		}
-		Vect3f getPosition() {
-			return position;
-		}
-		Vect3f getDiffuse() {
-			return diffuse;
-		}
-		Vect3f getSpecular() {
-			return specular;
-		}
-	private:
-		Vect3f specular;
-		Vect3f diffuse;
-		Vect3f position;
-
+public:
+    Light(Vect3f diffuse, Vect3f specular, Vect3f position) {
+        this->diffuse = diffuse;
+        this->specular = specular;
+        this->position = position;
+    }
+    Vect3f getPosition() {
+        return position;
+    }
+    Vect3f getDiffuse() {
+        return diffuse;
+    }
+    Vect3f getSpecular() {
+        return specular;
+    }
+private:
+    Vect3f specular;
+    Vect3f diffuse;
+    Vect3f position;
+    
 };
 void save_to_file(char *filename, int width, int height, Vect3f *pixels)
 {
-	// save frame buffer to file
-	std::ofstream ofs;
-	ofs.open("./out.ppm", std::ios::out | std::ios::binary);
-	ofs << "P6\n" << width << " " << height << "\n255\n";
-	for (int i = 0; i < height * width; ++i) {
-		ofs << (unsigned char)(min(max(pixels[i].getX(),0.0f), 1.0f)  * 255) <<
-			   (unsigned char)(min(max(pixels[i].getY(), 0.0f), 1.0f) * 255) <<
-			   (unsigned char)(min(max(pixels[i].getZ(), 0.0f), 1.0f) * 255);
-	}
-
-	ofs.close();
+    // save frame buffer to file
+    std::ofstream ofs;
+    ofs.open("./out.ppm", std::ios::out | std::ios::binary);
+    ofs << "P6\n" << width << " " << height << "\n255\n";
+    for (int i = 0; i < height * width; ++i) {
+        ofs << (unsigned char)(min(max(pixels[i].getX(),0.0f), 1.0f)  * 255) <<
+        (unsigned char)(min(max(pixels[i].getY(), 0.0f), 1.0f) * 255) <<
+        (unsigned char)(min(max(pixels[i].getZ(), 0.0f), 1.0f) * 255);
+    }
+    
+    ofs.close();
 }
 
 
@@ -253,16 +253,16 @@ Vect3f shade(Ray& ray, list<Object*>& objects, list<Light*>& lights, int depth) 
 
 	float tnear = INFINITY;
 	Object* closest = NULL;
-	Vect3f* intersection = &Vect3f();
+	Vect3f intersection;
 	//flag true when we hit something from this ray.
 	bool hit = false;
 	//for each object find intersection
 	//construct ray
-	for each (Object* obj in objects)
+    for(Object* obj : objects)
 	{
 		//current intersection;
 		float t;
-		if ((*obj).intersect(ray, *intersection, t)) {
+		if ((*obj).intersect(ray, intersection, t)) {
 			hit = true;
 			if (t < tnear) {
 				//we found a closer object.
@@ -275,25 +275,26 @@ Vect3f shade(Ray& ray, list<Object*>& objects, list<Light*>& lights, int depth) 
 		//set color to ambient light.
 		color = (*closest).getMaterial().getAmbientColor();
 		//set the surface normal.
-		Vect3f normal = (*closest).getNormalAtPoint((*intersection));
-		for each (Light* l in lights)
+		Vect3f normal = (*closest).getNormalAtPoint((intersection));
+        for(Light* l : lights)
 		{
 			//get the light direction.
-			Vect3f lightDir = ((*l).getPosition() - *intersection).normalized();
+			Vect3f lightDir = ((*l).getPosition() - intersection).normalized();
 			//reflected light vector.
 			//R_i - 2 N (R_i . N)
 			Vect3f reflectedLightvector = ((normal * 2 * lightDir.dot(normal)) - lightDir).normalized();
 
 			//******* SHADOW RAY ********\\
 			//get the light direction.
-			Vect3f startPosition = *intersection + lightDir * 0.0001f;
+			Vect3f startPosition = intersection + lightDir * 0.0001f;
 			Ray shadowRay(startPosition, lightDir);
 			bool hit = false;
-			for each (Object* obj in objects)
+            for(Object* obj : objects)
 			{
 				//if (obj == closest) continue;
 				float t;
-				if ((*obj).intersect(shadowRay, Vect3f(), t)) {
+                Vect3f tmp = Vect3f();
+				if ((*obj).intersect(shadowRay,tmp, t)) {
 					hit = true;
 				}
 			}
@@ -313,7 +314,7 @@ Vect3f shade(Ray& ray, list<Object*>& objects, list<Light*>& lights, int depth) 
 		}
 		if (depth >= 0) {
 			Vect3f reflection = (ray.getDirection() - normal * 2 * normal.dot(ray.getDirection())).normalized();
-			Ray reflectionRay = Ray(*intersection + reflection * 0.0001, reflection);
+			Ray reflectionRay = Ray(intersection + reflection * 0.0001, reflection);
 			color = color + shade(reflectionRay, objects, lights, depth - 1) * 0.5;
 		}
 	}
@@ -362,5 +363,4 @@ int main() {
 		save_to_file("test.bmp", camera.width, camera.height, pixels);
 
 		return 0;
-
 }
