d3d97e535a17379573a27bb5feac6484789229b3
Iwein Bau <iwein.bau@gmail.com>
2018-12-17 22:52:35 +0100

    Fixed issue with plane.

---
+++ Ray tracing/Ray tracing/Material.hpp
@@ -0,0 +1,13 @@
+//
+//  Material.hpp
+//  Ray-tracer
+//
+//  Created by Iwein Bau on 17/12/2018.
+//  Copyright © 2018 Iwein Bau. All rights reserved.
+//
+
+#ifndef Material_h
+#define Material_h
+
+
+#endif /* Material_h */
---
+++ Ray tracing/Ray tracing/Matte.hpp
@@ -0,0 +1,13 @@
+//
+//  Matte.hpp
+//  Ray-tracer
+//
+//  Created by Iwein Bau on 17/12/2018.
+//  Copyright © 2018 Iwein Bau. All rights reserved.
+//
+
+#ifndef Matte_h
+#define Matte_h
+
+
+#endif /* Matte_h */
