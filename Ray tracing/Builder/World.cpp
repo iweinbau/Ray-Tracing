6ca414d49a0743f8351b6ba87febd38a71266eea
Iwein Bau <iwein.bau@gmail.com>
2019-06-08 08:08:32 +0200

    changed sceen composition.

--- Ray tracing/Builder/World.cpp
+++ Ray tracing/Builder/World.cpp
@@ -17,6 +17,7 @@
 
 
 #include "../Objects/sphere.hpp"
+#include "../Objects/Rectangle.hpp"
 #include "../Objects/Plane.hpp"
 #include "../Objects/Instance.hpp"
 
@@ -35,14 +36,14 @@ void World::buildWorld(){
     //World setup
 
     //Directional* light = new Directional(Vect3(0, 1,0), Vect3(1),5);
-    Sphere* r = new Sphere(Vect3(0,4,0),1,new Emissive(10,Vect3(1)));
+    Rectangle* r = new Rectangle(Point3(-2.5,5,-2.5),Vect3(5,0,0),Vect3(0,0,5),new Emissive(10,Vect3(1)));
     AreaLight* light = new AreaLight(r,Vect3(1),10);
     r->setShadowCast(false);
     //PointLight* light = new PointLight(Vect3(1, 1,1), Vect3(-2,0, 2),10);
 
     add_Light(light);
     
-    ambientLight = AmbientLight(1,Vect3(1),1);
+    ambientLight = AmbientLight(0,Vect3(1),1);
 
     Reflective * reflective = new Reflective(Lambertian(0.25,Vect3(1,1,0.3)),
                                      new Lambertian(0.6,Vect3(1,1,0.3)),
@@ -60,15 +61,15 @@ void World::buildWorld(){
 
     Instance* s = new Instance(sphere,reflective);
     s->scale(Vect3(5));
-    s->translate(Vect3(0, 0, -8));
+    s->translate(Vect3(0, 0, -7));
 
     Instance* s2 = new Instance(sphere,Matte2);
     s2->scale(Vect3(2,3,2));
-    s2->translate(Vect3(-3, -2, -3));
+    s2->translate(Vect3(-3, -2, -2));
 
     Instance* s3 = new Instance(sphere,Matte3);
-    s3->scale(Vect3(3));
-    s3->translate(Vect3(3,-2, -2));
+    s3->scale(Vect3(2.5));
+    s3->translate(Vect3(3,-2, -1));
 
 
     Matte* planem = new Matte(
